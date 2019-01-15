96b46c5b523a146424217d16c46a20d60933c64e
Erin Green <erin.y.green@gmail.com>
2019-01-15 18:44:14 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -53,10 +53,8 @@ class LandmarksController < ApplicationController
     
   
     @landmark = Landmark.find(params[:id])
-    @landmark.update(params[:landmark])
-    
-    # @landmark.name = params['landmark']['name']
-    # @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
     @landmark.save
     redirect "landmarks/#{@landmark.id}"
   end
